0301f368052244882573d94b1a5c8f6d0ff5bec2
GET Academy <terje@getacademy.no>
2023-09-16 13:13:36 +0200

    x

--- Black jack/exerciseIndex.js
+++
@@ -1,9 +0,0 @@
-function countBlackJackPoints(cards) {
-    let points = 0;
-    for (let card of cards) {
-        let suitAndRank = card.split(' ');
-        let rank = suitAndRank[1];
-        points += parseInt(rank);
-    }
-    return points;
-}
--- Testris/controller.js
+++ Testris/controller.js
@@ -40,40 +40,20 @@ function addI(color) {
 }
 
 function rotate() {
-    /*
-        indeks, rad og kolonne for alle rutene på brettet: 
-        |-------------------------|-------------------------|-------------------------|-------------------------|
-        | indeks  0, col=0, rad=0 | indeks  1, col=1, rad=0 | indeks  2, col=2, rad=0 | indeks  3, col=3, rad=0 |
-        | indeks  4, col=0, rad=1 | indeks  5, col=1, rad=1 | indeks  6, col=2, rad=1 | indeks  7, col=3, rad=1 | 
-        | indeks  8, col=0, rad=2 | indeks  9, col=1, rad=2 | indeks 10, col=2, rad=2 | indeks 11, col=3, rad=2 | 
-        | indeks 12, col=0, rad=3 | indeks 13, col=1, rad=3 | indeks 14, col=2, rad=3 | indeks 15, col=3, rad=3 | 
-        |-------------------------|-------------------------|-------------------------|-------------------------|
-    */
     let tmpBoard = [];
     for (let index = 0; index < board.length; index++) {
-        let rowIndex;
-        if (index == 0 || index == 1 || index == 2 || index == 3) rowIndex = 0;
-        if (index == 4 || index == 5 || index == 6 || index == 7) rowIndex = 1;
-        if (index == 8 || index == 9 || index == 10 || index == 11) rowIndex = 2;
-        if (index == 12 || index == 13 || index == 14 || index == 15) rowIndex = 3;
-        let colIndex;
-        if (index == 0 || index == 4 || index == 8 || index == 12) colIndex = 0;
-        if (index == 1 || index == 5 || index == 9 || index == 13) colIndex = 1;
-        if (index == 2 || index == 6 || index == 10 || index == 14) colIndex = 2;
-        if (index == 3 || index == 7 || index == 11 || index == 15) colIndex = 3;
-        let newRowIndex;
-        if (colIndex == 0) newRowIndex = 0;
-        if (colIndex == 1) newRowIndex = 1;
-        if (colIndex == 2) newRowIndex = 2;
-        if (colIndex == 3) newRowIndex = 3;
-        let newColIndex;
-        if (rowIndex == 0) newColIndex = 3;
-        if (rowIndex == 1) newColIndex = 2;
-        if (rowIndex == 2) newColIndex = 1;
-        if (rowIndex == 3) newColIndex = 0;
-        let newIndex = newRowIndex * 4 + newColIndex;
+        const newIndex = getRotatedIndex(index);
         tmpBoard[newIndex] = board[index];
     }
     board = tmpBoard;
     updateView();
+}
+
+function getRotatedIndex(oldIndex){
+    let rowIndex = Math.floor(oldIndex / 4);
+    let colIndex = oldIndex % 4;
+    let newColIndex = 3 - rowIndex;
+    let newRowIndex = colIndex;
+    let newIndex = newRowIndex * 4 + newColIndex;
+    return newIndex;
 }
